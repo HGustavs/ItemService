f0346e7b2fbbcd97eba23215c668885263a34bbb
HGustavs <henrik.gustavsson@his.se>
2016-03-08 15:55:50 +0100

    Database fix

--- intemservice.sql
+++ intemservice.sql
@@ -2,15 +2,15 @@ Create Database Pilkington;
 Use Pilkington;
 
 CREATE TABLE items(
-	Itemid MEDIUMINT NOT NULL AUTO_INCREMENT,
-	Userid VARCHAR(16),
-	Itemname VARCHAR(64),
-	Sort VARCHAR(64),
-	Location VARCHAR(46),
-	Langd MEDIUMINT,
-	Pris DOUBLE (6,2),
-	Starttime TIMESTAMP,
-	Endtime TIMESTAMP,
-	Information TEXT,
-	PRIMARY KEY(Itemid)
+	itemid 		MEDIUMINT NOT NULL AUTO_INCREMENT,
+	userid 		VARCHAR(16),
+	itemname 	VARCHAR(64),
+	kind 			VARCHAR(64),
+	location 	VARCHAR(46),
+	size 			MEDIUMINT,
+	cost 			DOUBLE (6,2),
+	starttime TIMESTAMP,
+	endtime 	TIMESTAMP,
+	info 			TEXT,
+	PRIMARY KEY(itemid)
 );
